c720b82df0b38a4c9c08085e13b86f6ebed052ef
Eric Kiara <ekiara@gmail.com>
2023-04-14 14:40:14 +0300

    Add spiders for rankings page filtered on youth or women players

---
+++ chessonlinescraper/chessonlinescraper/spiders/chessonlinefemale.py
@@ -0,0 +1,30 @@
+import scrapy
+
+
+class ChessonlinefemaleSpider(scrapy.Spider):
+    """
+    Spider to retrieve the profile URLs of Female players
+    """
+
+    name = "chessonlinefemale"
+    allowed_domains = ["chess.com"]
+    start_urls = ["https://www.chess.com/ratings/women"]
+
+    def parse(self, response):
+        rows = response.css("table.table-component tbody tr")
+        for row in rows:
+            player_url: str = row.css("td")[1].css("div a").attrib["href"]
+            yield {
+                "player_url": player_url,
+            }
+
+        next_page = response.xpath(
+            '//*[@id="view-master-players"]/div/div[2]/div/nav/a[6]'
+        ).attrib["href"]
+        if next_page != response.url:
+            yield scrapy.Request(
+                url=next_page,
+                callback=self.parse,
+            )
+        else:
+            print(f"Reached the last page: {next_page}")
